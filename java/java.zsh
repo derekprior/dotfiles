13bdd07b0711507a78d8d4f3f1b8f61bead6a9d8
Derek Prior <derekprior@gmail.com>
2012-04-20 10:12:40 -0400

    Added Java. Consolidated ruby zsh stuff and added JRUBY opts

---
+++ java/java.zsh
@@ -0,0 +1,4 @@
+if [ -d /Library/Java/JavaVirtualMachines/1.7.0.jdk ]; then
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home
+fi
+
--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,3 +1,11 @@
+# Rails
 alias r='rails'
 alias tl='tail -f log/development.log'
 alias dbm='rake db:migrate'
+
+# Bundler
+alias be='bundle exec'
+
+# JRuby
+export JRUBY_OPTS=--1.9
+
