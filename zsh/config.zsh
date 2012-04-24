b22bd7e52928e46cd7dfab9e62359ebd36273d7a
Derek Prior <derekprior@gmail.com>
2012-04-24 09:56:48 -0400

    enabled auto_cd

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -22,6 +22,7 @@ setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
+setopt AUTO_CD
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
