e595162acd3e35d6e7613f4241dc41bd540eb299
Derek Prior <derekprior@gmail.com>
2013-03-05 16:41:18 -0500

    cleaning up functions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -47,6 +47,10 @@ unsetopt correct
 
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*' group-name ''
