d7d0a2298f30e9a527595d5772979ad15ae453f3
Derek Prior <derekprior@gmail.com>
2013-03-05 15:57:48 -0500

    simplify zsh config a bit

--- ruby/ruby.zsh
+++ ruby/ruby.zsh
@@ -1,17 +1,9 @@
-# Rails
 alias r='rails'
-alias tl='tail -f log/development.log'
 alias dbm='rake db:migrate'
-
-# Bundler
-alias b='bundle'
 alias be='bundle exec'
 
 # JRuby
 export JRUBY_OPTS=--1.9
-
-# Tests
-alias cuc="bundle exec cucumber"
 
 # Use Zeus for rspec if it's running
 rspec() {
