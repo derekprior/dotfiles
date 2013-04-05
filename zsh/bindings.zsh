448a5886880fdd5d3917cda7e8661120cea2d67e
Derek Prior <derekprior@gmail.com>
2013-04-05 11:11:39 -0400

    zsh key bindings

---
+++ zsh/bindings.zsh
@@ -0,0 +1,7 @@
+bindkey -v
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+bindkey "^P" history-search-backward
+bindkey "^R" history-incremental-search-backward
+bindkey "^N" insert-last-word
+bindkey jj vi-cmd-mode
