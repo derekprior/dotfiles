8029bad514716babef9eafc3b9f7ec2bc10e0b60
Derek Prior <derekprior@gmail.com>
2013-07-16 09:13:22 -0400

    Make migrate faster

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,7 +10,7 @@ alias mkdir='mkdir -p'
 alias ...='../..'
 
 alias be='bundle exec'
-alias migrate="rake db:migrate && rake db:rollback && rake db:migrate && rake db:test:prepare"
+alias migrate="rake db:migrate db:rollback && rake db:migrate db:test:prepare"
 alias m="migrate"
 alias r='rails'
 alias rk="rake"
