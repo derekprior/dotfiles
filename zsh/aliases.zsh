d7d32f4fb99a44f5d3eb945961f525534f991924
Derek Prior <derekprior@gmail.com>
2013-11-29 11:51:35 -0500

    Vagrant tweeks.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,7 @@ alias ls='ls -lGh'
 alias la='ls -lGha'
 alias fixterm='sudo rm -rf /private/var/log/asl/*.asl'
 alias flushdns='dscacheutil -flushcache'
-alias tlf="tail -f"
+alias tlf='tail -f'
 alias less='less -R'
 alias ln='ln -v'
 alias mkdir='mkdir -p'
@@ -22,7 +22,7 @@ alias tu='ruby -Itest'
 alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
-if type hub > /dev/null 2>&1; then alias git="hub"; fi
+if type hub > /dev/null 2>&1; then alias git='hub'; fi
 alias g='git'
 alias gs='git status -sb'
 alias ga='git add'
@@ -35,10 +35,11 @@ alias gd='git diff'
 alias h='heroku'
 alias herok='heroku'
 
-alias tmux="tmux -2"
+alias tmux='tmux -2'
 
-alias vssh="vagrant ssh"
-alias vsh="vagrant ssh"
-alias vup="vagrant up"
+alias v='vagrant'
+alias vssh='vagrant ssh'
+alias vsh='vagrant ssh'
+alias vup='vagrant up'
 
-alias dotfiles="cd $DOTFILES"
+alias dotfiles='cd $DOTFILES'
