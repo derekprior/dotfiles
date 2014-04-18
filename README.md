d1dab25625a84d1c53b862cb95f89891e68e421d
Derek Prior <derekprior@gmail.com>
2014-04-18 16:09:14 -0400

    Switch to thoughtbot dotfiles
    Most of my dotfiles have been trending that way anyway, so I have
    decided to go all in. This repo now has my extensions and overrides to
    those. I will evenalutate these extensions to see if they are more
    appropriate as part of the thoughtbot dotfiles themselves.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-My dotfiles
+# My Dotfiles
+
+My overrides to the [thoughtbot dotfiles][1], installed with [rcm][2].
+
+[1]:https://github.com/thoughtbot/dotfiles
+[2]:https://github.com/thoughtbot/rcm
--- zsh/bindings.zsh
+++
@@ -1,7 +0,0 @@
-bindkey -v
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^P" history-search-backward
-bindkey "^R" history-incremental-search-backward
-bindkey "^N" insert-last-word
-bindkey jj vi-cmd-mode
