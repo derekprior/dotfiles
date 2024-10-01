c3435e355ea75afa56ba4b02f0d8e471058f5f1c
Derek Prior <derekprior@gmail.com>
2024-09-30 23:23:48 -0400

    Add pane resizing for wezterm
    I wanted to be able to hit `LEADER` and then repeat various directions
    within a timeout to play with the sizing, similar to `bind -r` in
    `tmux`. The key to doing this is Key Tables, and `action.Multiple`.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,42 +1,80 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local wez_action = wezterm.action
 
-config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1000 }
+-- Leader is CTRL + s, like tmux
+config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 500 }
+
+resize_opts = {
+  name = "resize_mode",
+  one_shot = false,
+  timeout_milliseconds = 1000,
+  until_unknown = true,
+  replace_current = true,
+}
+
+enter_resize_mode = function(direction)
+  return wez_action.Multiple {
+    wez_action.AdjustPaneSize { direction, 1 },
+    wez_action.ActivateKeyTable {
+      name = "resize_mode",
+      one_shot = false,
+      timeout_milliseconds = 500,
+      until_unknown = true,
+      replace_current = true,
+    },
+  }
+end
+
+config.key_tables = {
+  resize_mode = {
+    { key = "h", action = wez_action.AdjustPaneSize { "Left", 1 } },
+    { key = "j", action = wez_action.AdjustPaneSize { "Down", 1 } },
+    { key = "k", action = wez_action.AdjustPaneSize { "Up", 1 } },
+    { key = "l", action = wez_action.AdjustPaneSize { "Right", 1 } },
+  },
+}
+
 config.keys = {
   -- Splitting panes
   {
     mods   = "LEADER",
     key    = "-",
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    action = wez_action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
     mods   = "LEADER",
     key    = "\\",
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    action = wez_action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
   -- Zoom pane
   {
     mods   = "LEADER",
     key    = "z",
-    action = wezterm.action.TogglePaneZoomState,
+    action = wez_action.TogglePaneZoomState,
   },
   -- rotate panes
   {
     mods = "LEADER",
     key = "}",
-    action = wezterm.action.RotatePanes "Clockwise",
+    action = wez_action.RotatePanes "Clockwise",
   },
   {
     mods = "LEADER",
     key = "{",
-    action = wezterm.action.RotatePanes "CounterClockwise",
+    action = wez_action.RotatePanes "CounterClockwise",
   },
   -- activate copy mode or vim mode
   {
     key = 'Enter',
     mods = 'LEADER',
-    action = wezterm.action.ActivateCopyMode
-  }
+    action = wez_action.ActivateCopyMode
+  },
+  -- resize panes
+  { key = "h", mods = "LEADER", action = enter_resize_mode("Left")},
+  { key = "j", mods = "LEADER", action = enter_resize_mode("Down")},
+  { key = "k", mods = "LEADER", action = enter_resize_mode("Up")},
+  { key = "l", mods = "LEADER", action = enter_resize_mode("Right")},
 }
 
 -- Tab switching by number
@@ -44,7 +82,7 @@ for i = 1, 9 do
   table.insert(config.keys, {
     key = tostring(i),
     mods = 'LEADER',
-    action = wezterm.action.ActivateTab(i-1),
+    action = wez_action.ActivateTab(i-1),
   })
 end
 
