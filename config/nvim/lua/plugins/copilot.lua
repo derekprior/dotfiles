d7f3ae2beddeec5dd7ae3d65bd481704f72f725d
Derek Prior <derekprior@gmail.com>
2024-10-03 10:38:07 -0400

    Add basic copilot configuration for nvim

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = { "Copilot" },
+    build = ":Copilot auth",
+    config = function()
+      require("copilot").setup({
+        suggestion = { auto_trigger = "true" },
+        filetypes = { markdown = true, gitcommit = true },
+      })
+    end
+  },
+  {
+    "AndreM222/copilot-lualine",
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            { "copilot", show_colors = true },
+            "fileformat",
+            "filetype"
+          },
+        },
+      })
+    end
+  },
+}
