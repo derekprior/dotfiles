423330df1ac267bb7940c8925adb0015e18e2e28
Derek Prior <derekprior@gmail.com>
2024-10-03 21:03:01 -0400

    Add Blake's completion configs

--- config/nvim/lua/plugins/completions.lua
+++ config/nvim/lua/plugins/completions.lua
@@ -1,44 +1,102 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+
+      return {
+        auto_brackets = {},
+        completion = { completeopt = "menu,menuone,preview,popup,noinsert" },
+        preselect = cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<C-Space>'] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
+          { name = "path" },
+          { name = 'nvim_lsp_signature_help' },
         }, {
           { name = "buffer" },
         }),
+        formatting = {
+          format = function(_, item)
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
+
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    init = function()
+      local cmp = require("cmp")
+      cmp.setup.cmdline(":", {
+        formatting = { fields = { "abbr" } },
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
       })
     end,
-  },
+  }
 }
--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,28 +1,46 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		cmd = { "Copilot" },
-		build = ":Copilot auth",
-		config = function()
-			require("copilot").setup({
-				suggestion = { auto_trigger = "true" },
-				filetypes = { markdown = true, gitcommit = true },
-			})
-		end,
-	},
-	{
-		"AndreM222/copilot-lualine",
-		config = function()
-			require("lualine").setup({
-				sections = {
-					lualine_x = {
-						{ "copilot", show_colors = true },
-						"fileformat",
-						"filetype",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = { "Copilot" },
+    build = ":Copilot auth",
+    config = function()
+      require("copilot").setup({
+        suggestion = { auto_trigger = "true" },
+        filetypes = { markdown = true, gitcommit = true },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      lsp_on_attach(function(_)
+        copilot_cmp._on_insert_enter({})
+      end, "copilot")
+    end,
+
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+  {
+    "AndreM222/copilot-lualine",
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            { "copilot", show_colors = true },
+            "fileformat",
+            "filetype",
+          },
+        },
+      })
+    end,
+  },
 }
