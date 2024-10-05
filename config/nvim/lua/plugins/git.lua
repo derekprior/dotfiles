4410eec61e44c9c9eeb140c0fc87ff096cbb8278
Derek Prior <derekprior@gmail.com>
2024-10-04 22:56:36 -0400

    Add more to my nvim git setup
    Bring in fugitive and rhubarb to fulfill muscle memory for blame,
    `GBrowse`, `Gread`, etc. There may be better pure lua implementation out
    there, but these have worked for over a decade.
    
    Added Gitsigns to mark changes in the gutter.

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,15 +1,41 @@
 return {
-  "kdheepak/lazygit.nvim",
-  lazy = true,
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>lg", "<CMD>LazyGit<CR>", desc = "LazyGit" },
+    },
   },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  {
+    "tpope/vim-fugitive",
+    cmd = { "G", "Git", "GBrowse" },
+    keys = {
+      { "<leader>gb", "<CMD>Git blame<CR>", desc = "git blame" },
+    },
+  },
+  {
+    "tpope/vim-rhubarb",
+    cmd = { "GBrowse" },
+    keys = {
+      { "<leader>go", "<CMD>GBrowse<CR>", desc = "git browse" },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    cmd = { "Gitsigns" },
+    opts = {},
+    lazy = false,
+    keys = {
+      { "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", desc = "git diff hunk" },
+      { "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>",   desc = "git reset diff hunk" },
+      { "<leader>gR", "<CMD>Gitsigns reset_buffer<CR>", desc = "git reset buffer" },
+    },
   },
 }
--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.diagnostics.proselint,
-			},
-		})
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
-	end,
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format buffer" })
+  end,
 }
