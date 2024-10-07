d0f20f1e3e7cd516cdd1e92bb674960b311966c7
Derek Prior <derekprior@gmail.com>
2024-10-07 15:08:39 -0400

    Update telescope configuration
    Add some menu navigation and smart path display for projects with deep
    heierarchies.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,22 @@ return {
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
 			local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            }
+          }
+        }
+      })
 
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
