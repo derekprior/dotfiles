17c8ec128ce72719ab72b4c426a978da010e5391
Derek Prior <derekprior@gmail.com>
2024-10-07 15:30:39 -0400

    Enable Lazy update checking
    This will display an item in Lualine when my plugins have updates.
    
    I don't love that I tacked this into the `copilot-lualine` config but I
    was already modifying the lualine there and I wasn't sure how updating
    it in multiple places would "stack".

--- config/nvim/lua/lazy-setup.lua
+++ config/nvim/lua/lazy-setup.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", { change_detection = { notify = false }})
+require("lazy").setup("plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,26 +1,38 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		cmd = { "Copilot" },
-		build = ":Copilot auth",
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    cmd = { "Copilot" },
+    build = ":Copilot auth",
     opts = {
       suggestion = { auto_trigger = "true" },
       filetypes = { markdown = true, gitcommit = true },
     },
-	},
-	{
-		"AndreM222/copilot-lualine",
-		config = function()
-			require("lualine").setup({
-				sections = {
-					lualine_x = {
-						{ "copilot", show_colors = true },
-						"fileformat",
-						"filetype",
-					},
-				},
-			})
-		end,
-	},
+  },
+  {
+    "AndreM222/copilot-lualine",
+    config = function()
+      local lualine = require("lualine")
+      local lazy_status = require("lazy.status")
+
+
+      lualine.setup({
+        sections = {
+          lualine_x = {
+            {
+              lazy_status.updates,
+              cond = lazy_status.has_updates,
+              color = { fg = "#ff9e64" },
+            },
+            {
+              "copilot",
+              show_colors = true
+            },
+            "fileformat",
+            "filetype",
+          },
+        },
+      })
+    end,
+  },
 }
