e46e8555795bf38cf8bec9841a66ae038804f50e
Derek Prior <derekprior@gmail.com>
2024-10-09 08:59:39 -0400

    Remove Copilot status from lualine
    Not something I need to see constantly.

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,17 @@
 return {
 	"nvim-lualine/lualine.nvim",
-  opts = { theme = "dracula" },
+	opts = {
+		theme = "dracula",
+		sections = {
+			lualine_x = {
+				{
+					require("lazy.status").updates,
+					cond = require("lazy.status").has_updates,
+					color = { fg = "#ff9e64" },
+				},
+				"fileformat",
+				"filetype",
+			},
+		},
+	},
 }
