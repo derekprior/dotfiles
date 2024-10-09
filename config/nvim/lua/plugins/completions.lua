9496f2ae4537073bd55417a91b847d7f08e6d196
Derek Prior <derekprior@gmail.com>
2024-10-09 09:32:32 -0400

    Update copilot and completion configs
    Copilot is essentially another type of completion, so I moved it into my
    completion config. I also updated `nvim.cmp` so that `<Tab>` behaves
    _almost_ exactly as I'd like -- it will accept the copilot suggestion if
    it's present, otherwise it will cycle through the completion options.
    
    We'll play with this for a bit and see how it goes.

--- config/nvim/lua/plugins/completions.lua
+++ config/nvim/lua/plugins/completions.lua
@@ -1,5 +1,23 @@
 return {
 	{
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		cmd = { "Copilot" },
+		build = ":Copilot auth",
+		opts = {
+			panel = {
+				enabled = false,
+			},
+			suggestion = {
+				auto_trigger = "true",
+			},
+			filetypes = {
+				markdown = true,
+				gitcommit = true,
+			},
+		},
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
@@ -29,31 +47,47 @@ return {
 						luasnip.lsp_expand(args.body)
 					end,
 				},
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						local copilot_suggestion = require("copilot.suggestion")
+
+						if copilot_suggestion.is_visible() then
+							copilot_suggestion.accept()
+						elseif cmp.visible() then
+							local entry = cmp.get_selected_entry()
+
+							if not entry then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+							else
+								cmp.confirm()
+							end
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
-          { name = "nvim_lsp" },
+					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "buffer" },
 					{ name = "path" },
 				}),
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = "...",
-          })
-        },
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
 			})
 		end,
 	},
