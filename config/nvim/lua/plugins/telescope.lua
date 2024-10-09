555e1f617a52f1add627c395cb03b3e7d6df9df7
Derek Prior <derekprior@gmail.com>
2024-10-08 21:38:03 -0400

    Send all results to qf if none are explicitly selected
    Instead of having to select all the results (with tab), we can send an
    empty selection that will just send all.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
             i = {
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-j>"] = actions.move_selection_next,
-              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
             }
           }
         }
