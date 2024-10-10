10be91090f9ca7d3b24f901a577c87e57b458679
Derek Prior <derekprior@gmail.com>
2024-10-10 10:43:20 -0400

    Switch to modern layout for which-key
    I like helix, but when I'm operating in full screen mode, it can be
    really hard to even notice it's down there in the lower right!

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,20 +1,20 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    preset = "helix",
-    marks = true,
-    registers = true,
-    spelling = { enabled = true },
-    show_help = false,
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "modern",
+		marks = true,
+		registers = true,
+		spelling = { enabled = true },
+		show_help = false,
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
