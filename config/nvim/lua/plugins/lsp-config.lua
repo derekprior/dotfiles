0cdc582b1ca275eb65874f3cbedfdd6dd77be090
Derek Prior <derekprior@gmail.com>
2026-02-05 13:54:32 -0500

    Quiet lspconfig issues in new versions of neovim for now

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -1,98 +1,98 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-		},
-		config = function()
-			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
-			local mason_tool_installer = require("mason-tool-installer")
-			mason.setup({})
-
-			mason_lspconfig.setup({
-				ensure_installed = {
-					"cssls",
-					"html",
-					"lua_ls",
-					"ts_ls",
-				},
-			})
-
-			mason_tool_installer.setup({
-				ensure_installed = {
-					"prettier",
-					"stylua",
-					"eslint_d",
-					"proselint",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			{ "antosha417/nvim-lsp-file-operations", config = true },
-			{ "folke/neodev.nvim", opts = {} },
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			local mason_lspconfig = require("mason-lspconfig")
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local keymap = vim.keymap
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(event)
-					local opts = { buffer = event.buf, silent = true }
-
-					opts.desc = "Show LSP references"
-					keymap.set("n", "gR", "<cmd>FzfLua lsp_references<CR>", opts)
-
-					opts.desc = "Go to declaration"
-					keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-					opts.desc = "Show LSP definitions"
-					keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts)
-
-					opts.desc = "Show LSP implementations"
-					keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", opts)
-
-					opts.desc = "Show LSP type definitions"
-					keymap.set("n", "gt", "<cmd>FzfLua lsp_type_definitions<CR>", opts)
-
-					opts.desc = "See available code actions"
-					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-					opts.desc = "Smart rename"
-					keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-
-					opts.desc = "Show line diagnostics"
-					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-
-					opts.desc = "Go to previous diagnostic"
-					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-					opts.desc = "Go to next diagnostic"
-					keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-					opts.desc = "Show documentation for what is under cursor"
-					keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				end,
-			})
-
-			local capabilities = cmp_nvim_lsp.default_capabilities()
-
-			mason_lspconfig.setup_handlers({
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
-		end,
-	},
+	-- {
+	-- 	"williamboman/mason.nvim",
+	-- 	dependencies = {
+	-- 		"williamboman/mason-lspconfig.nvim",
+	-- 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- 	},
+	-- 	config = function()
+	-- 		local mason = require("mason")
+	-- 		local mason_lspconfig = require("mason-lspconfig")
+	-- 		local mason_tool_installer = require("mason-tool-installer")
+	-- 		mason.setup({})
+	--
+	-- 		mason_lspconfig.setup({
+	-- 			ensure_installed = {
+	-- 				"cssls",
+	-- 				"html",
+	-- 				"lua_ls",
+	-- 				"ts_ls",
+	-- 			},
+	-- 		})
+	--
+	-- 		mason_tool_installer.setup({
+	-- 			ensure_installed = {
+	-- 				"prettier",
+	-- 				"stylua",
+	-- 				"eslint_d",
+	-- 				"proselint",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		{ "antosha417/nvim-lsp-file-operations", config = true },
+	-- 		{ "folke/neodev.nvim", opts = {} },
+	-- 	},
+	-- 	config = function()
+	-- 		local lspconfig = require("lspconfig")
+	-- 		local mason_lspconfig = require("mason-lspconfig")
+	-- 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	-- 		local keymap = vim.keymap
+	--
+	-- 		vim.api.nvim_create_autocmd("LspAttach", {
+	-- 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	-- 			callback = function(event)
+	-- 				local opts = { buffer = event.buf, silent = true }
+	--
+	-- 				opts.desc = "Show LSP references"
+	-- 				keymap.set("n", "gR", "<cmd>FzfLua lsp_references<CR>", opts)
+	--
+	-- 				opts.desc = "Go to declaration"
+	-- 				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	--
+	-- 				opts.desc = "Show LSP definitions"
+	-- 				keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts)
+	--
+	-- 				opts.desc = "Show LSP implementations"
+	-- 				keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", opts)
+	--
+	-- 				opts.desc = "Show LSP type definitions"
+	-- 				keymap.set("n", "gt", "<cmd>FzfLua lsp_type_definitions<CR>", opts)
+	--
+	-- 				opts.desc = "See available code actions"
+	-- 				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+	--
+	-- 				opts.desc = "Smart rename"
+	-- 				keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+	--
+	-- 				opts.desc = "Show line diagnostics"
+	-- 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+	--
+	-- 				opts.desc = "Go to previous diagnostic"
+	-- 				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+	--
+	-- 				opts.desc = "Go to next diagnostic"
+	-- 				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	--
+	-- 				opts.desc = "Show documentation for what is under cursor"
+	-- 				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	-- 			end,
+	-- 		})
+	--
+	-- 		local capabilities = cmp_nvim_lsp.default_capabilities()
+	--
+	-- 		mason_lspconfig.setup({
+	-- 			function(server_name)
+	-- 				lspconfig[server_name].setup({
+	-- 					capabilities = capabilities,
+	-- 				})
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
 }
